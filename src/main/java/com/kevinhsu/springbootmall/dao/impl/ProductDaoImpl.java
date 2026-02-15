20de4398680ed9d7e08670c1ffb4694a9c1e73b2
康恆 <wa0125678665@gmail.com>
2026-02-16 07:01:08 +0800

    商品功能-實作修改商品

--- src/main/java/com/kevinhsu/springbootmall/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/kevinhsu/springbootmall/dao/impl/ProductDaoImpl.java
@@ -49,13 +49,13 @@ public class ProductDaoImpl implements ProductDao {
     public Integer createProduct(ProductRequest productRequest) {
         String sql = "INSERT INTO product(product_name,category,image_url,price,stock, " +
                 "description,created_date,last_modified_date)"+
-                "VALUES (:productName, :category, :imageurl, :price, :stock, :description, "+
+                "VALUES (:productName, :category, :imageUrl, :price, :stock, :description, "+
                 ":createdDate, :lastModifiedDate)";
 
         Map<String,Object> map = new HashMap<>();
         map.put("productName", productRequest.getProductName());
         map.put("category", productRequest.getCategory().toString());
-        map.put("imageurl", productRequest.getImageUrl());
+        map.put("imageUrl", productRequest.getImageUrl());
         map.put("price", productRequest.getPrice());
         map.put("stock", productRequest.getStock());
         map.put("description", productRequest.getDescription());
@@ -71,5 +71,26 @@ public class ProductDaoImpl implements ProductDao {
         int productId = keyHolder.getKey().intValue();
 
         return productId;
+    }
+
+    @Override
+    public void updateProduct(Integer productId, ProductRequest productRequest) {
+        String sql = "UPDATE product SET product_name = :productName, category=:category, image_url = :imageUrl," +
+                "price = :price,stock = :stock, description = :description, last_modified_date = :lastModifiedDate" +
+                " WHERE product_id = :productId";
+
+        Map<String,Object> map = new HashMap<>();
+        map.put("productId", productId);
+
+        map.put("productName", productRequest.getProductName());
+        map.put("category", productRequest.getCategory().toString());
+        map.put("imageUrl", productRequest.getImageUrl());
+        map.put("price", productRequest.getPrice());
+        map.put("stock", productRequest.getStock());
+        map.put("description", productRequest.getDescription());
+
+        map.put("lastModifiedDate", new Date());
+
+        namedParameterJdbcTemplate.update(sql, map);
     }
 }
